47cc2e1d34b88d70fcefe0f2bb8fe83613723b28
Venizio <gabinpincon@gmail.com>
2024-09-11 15:46:17 +0200

    [ADD]Troubabook

--- Team36_GodFatherMother_2024/Assets/Scripts/Vitrail/VitrailXP.cs
+++ Team36_GodFatherMother_2024/Assets/Scripts/Vitrail/VitrailXP.cs
@@ -2,19 +2,42 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class VitrailXP : MonoBehaviour
 {
-    private Window _window;
-    public void OpenTroubadour()
+    [SerializeField] private Window _window;
+    public void OpenTroubadourButton()
     {
-        _window.Windows[0].ShowWindow(_window.Windows[0]);
+        Debug.Log("I open toubadour");
+        _window.Windows[0]?.ShowWindow();
     }
 
-    public void OpenWiki()
+    public void OpenWikiButton()
     {
-        _window.Windows[1].ShowWindow(_window.Windows[1]);
+        Debug.Log("I open Wiki");
+        _window.Windows[1]?.ShowWindow();
+    }
+
+    public void OpenMatriculeButton()
+    {
+        Debug.Log("I open Matricule");
+        _window.Windows[2]?.ShowWindow();
+    }
+
+    public void OpenVerdictButton()
+    {
+        Debug.Log("I open Verdict");
+        _window.Windows[3]?.ShowWindow();
+    }
+
+    public void CloseAllWindowsButton()
+    {
+        foreach (var item in _window.Windows)
+        {
+            item.gameObject.SetActive(false);
+        }
     }
 }
