6e9367b96f4978e80323f7ff79555789a842f858
Kevin Vu-Morello <90256389+Shirotetsu@users.noreply.github.com>
2024-09-12 12:25:22 +0200

    Verdict category with selection and visual feedback / Validation Set up

---
+++ Team36_GodFatherMother_2024/Assets/CategoryType.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CategoryType : MonoBehaviour
+{
+    [SerializeField]
+    private Image backgroundBorderColor;
+
+    [SerializeField]
+    private PotionType potionType;
+
+    public void ChangeColorToSelected()
+    {
+        backgroundBorderColor.color = Color.green;
+    }
+
+    public void ChangeColorToUnSelected()
+    {
+        backgroundBorderColor.color = Color.black;
+    }
+}
--- Team36_GodFatherMother_2024/Assets/Verdict.cs
+++ Team36_GodFatherMother_2024/Assets/Verdict.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Verdict : Window
 {
-    // Start is called before the first frame update
+    private CategoryType categorySelected;
+
     void Start()
     {
         this.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Test(CategoryType categoryType)
     {
-        
+        if (categorySelected != null)
+        {
+            categorySelected.ChangeColorToUnSelected();
+        }
+
+        categorySelected = categoryType;
+
+        categoryType.ChangeColorToSelected();
+    }
+    public void ValidationButton(Image borderColor)
+    {
+        if (categorySelected == null)
+            return;
+
+        borderColor.color = Color.green;
+
+        Debug.Log("Validation");
     }
 }
